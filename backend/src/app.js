13ebb19c65880d4e68087d0f59d9b4b7128d0e6d
Adarsh Singh <asingh003.btech2024@cse.nitrr.ac.in>
2026-07-07 09:49:54 +0530

    Prepare backend for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,13 +13,18 @@ const searchRoutes = require("./routes/searchRoutes");
 
 const app = express();
 
-app.use(cors());
+// CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/study-plans", studyPlanRoutes);
 app.use("/api/questions", questionRoutes);
@@ -28,6 +33,8 @@ app.use("/api/analytics", analyticsRoutes);
 app.use("/api/resume", resumeRoutes);
 app.use("/api/mock-interview", mockInterviewRoutes);
 app.use("/api/search", searchRoutes);
+
+// Health Check
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     success: true,
